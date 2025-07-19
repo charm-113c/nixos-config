557384b9f75f22fa9429006311534710b7f994f6
Charm-113c <charles.newton.h@gmail.com>
2025-07-19 13:13:51 +0200

    Regular update, riced lock screen

--- home/hypr/hyprlock.nix
+++ home/hypr/hyprlock.nix
@@ -3,46 +3,80 @@
   programs.hyprlock = {
     enable = true;
     settings = {
+
+      general = {
+        ignore_empty_input = 1;
+        grace = 10; # period in s after lock where mouse movement unlocks automatically
+      };
+
+      animation = [
+          "fade, 1, 8, linear"
+      ];
+
       background = {
           path = "~/.dotfiles/home/hypr/Wallpapers/9.jpg";
 # "~/.dotfiles/home/hypr/Wallpapers/bioluminescence-5120x2880-15292.jpg";
           blur_passes = 0;
           blur_size = 4;
-          grace = 20; # period in s after lock where mouse movement unlocks automatically
         };
 
       label = [
         {
           # Clock module
           text = "$TIME";
+          # font_family = "Hurmit";
+          color = "rgba(255, 255, 255, 0.7)";
           halign = "center";
           valign = "center";
-          position = "0, 200";
+          position = "-10, -200";
           font_size = 40;
+          shadow_passes = 4;
+          shadow_color = "rgba(0, 0, 0, 0.5)";
           }
 
         {
           # Message module
           text = "Yo, Harmony.";
+          color = "rgba(255, 255, 255, 0.7)";
           halign = "center";
           valign = "center";
-          position = "0, 0";
+          position = "-10, -275";
+          shadow_passes = 2;
+          shadow_color = "rgba(0, 0, 0, 0.5)";
           }
+      ];
+
+      # Decorative shape around texts
+      shape = [
+        {
+          position = "-10, -235";
+          halign = "center";
+          valign = "center";
+          size = "400, 200";
+          color = "rgba(0,0,0,0.5)";
+          rounding = 5;
+          border_size = 2;
+          border_color = "rgba(39, 230, 255, 0.5)";
+          shadow_passes = 4;
+          shadow_size = 10;
+          shadow_color = "rgb(0, 0, 0)";
+        }
       ];
 
       input-field = [
         {
           size = "500, 40";
-          position = "0, -100";
+          position = "-10, -90";
+          font_color = "rgba(255, 255, 255, 0.7)";
           monitor = "";
           dots_center = true;
-          fade_on_empty = false;
+          fade_on_empty = true;
           # font_color = "rgb(241, 221, 30)";
-          inner_color = "rgba(39, 230, 255, 0.2)";
-          outer_color = "rgba(39, 230, 255, 0.2)";
+          inner_color = "rgba(0,0,0,0.2)";
+          outer_color = "rgba(39, 230, 255, 0.5)";
           outline_thickness = 2;
           placeholder_text = "Enter password";
-          shadow_passes = 2;
+          # shadow_passes = 2;
         }
       ];
     };
