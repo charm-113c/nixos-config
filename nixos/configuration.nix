9e4c8cf50e647d52a9a995935ab25198102fe538
Charm-113c <charles.newton.h@gmail.com>
2025-10-27 11:13:19 +0100

    Regular update, fcitx still not fixed, and there's been an issue with booting since the 18/10 update.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,10 +58,13 @@
   # i18n.inputMethod = {
   #   type = "fcitx5";
   #   enable = true;
-  #   fcitx5.addons = with pkgs; [
-  #     fcitx5-mozc
-  #     fcitx5-gtk
-  #   ];
+  #   fcitx5 = {
+  #     waylandFrontend = true;
+  #     addons = with pkgs; [
+  #       fcitx5-mozc
+  #       # fcitx5-gtk
+  #     ];
+  #   };
   # };
 
   # Enable X11 windowing system
