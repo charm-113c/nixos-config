d29c477f2632dbbe172e625da46146aebe4ba18b
Charm-113c <charles.newton.h@gmail.com>
2025-10-29 14:33:00 +0100

    Regular update

--- home/scripts/wallpaper.sh
+++ home/scripts/wallpaper.sh
@@ -3,7 +3,7 @@
 
 prefix="$HOME/.dotfiles/home/hypr/Wallpapers"
 # Declare all wanted wallpapers here
-wallpapers=("${prefix}/129850683_p0.png" "${prefix}/116644974_p0.png" "${prefix}/131409687_p0.jpg" "${prefix}/1358147.png" "${prefix}/135100174_p0.jpg" "${prefix}/1310702.png" "${prefix}/104651378_p0.jpg")
+wallpapers=("${prefix}/129850683_p0.png" "${prefix}/127659704_p1.png" "${prefix}/131409687_p0.jpg" "${prefix}/93892148_p0.jpg" "${prefix}/135100174_p0.jpg" "${prefix}/1310201.jpeg" "${prefix}/104651378_p0.jpg" "${prefix}/136221169_p0.jpg")
 
 # Roll the die
 i=$((RANDOM % ${#wallpapers[@]}))
