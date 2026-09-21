28635e1a87664380befe37d01c902434544aba15
Charm-113c <charles.newton.h@gmail.com>
2026-09-21 18:40:29 +0200

    Regular update

--- home/hypr/hyprland.lua
+++ home/hypr/hyprland.lua
@@ -3,7 +3,7 @@ hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("GBM_BACKEND", "nvidia-drm")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("AQ_DRM_DEVICES", "/dev/dri/card2:/dev/dri/card1")
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card1:/dev/dri/card0")
 hl.env("NIXOS_OZONE_WL", "1")
 
 hl.on("hyprland.start", function()
